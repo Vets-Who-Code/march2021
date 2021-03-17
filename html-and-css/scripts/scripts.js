4eb35cc47fbdeb4596cdec963e5dee8cd572973c
Stephan Lamoureux <sa.lamoureux@gmail.com>
2021-03-17 02:12:25 -0400

    started nav menu

--- html-and-css/scripts/scripts.js
+++ html-and-css/scripts/scripts.js
@@ -61,3 +61,23 @@ function screamSound() {
 }
 
 //Modal End//
+
+// Hamburger Menu
+
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu");
+
+menu.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    navbar.classList.toggle("showNav");
+    menu.classList.toggle("showClose");
+}
+
+const menuLinks = document.querySelectorAll(".menu-link");
+
+menuLinks.forEach(
+    function (menuLink) {
+        menuLink.addEventListener("click", toggleMenu);
+    }
+);
