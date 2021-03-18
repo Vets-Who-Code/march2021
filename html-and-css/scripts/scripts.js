77117eff4e07db490d066223a79d72c5b039cc0e
David Tetreau <david_tetreau@yahoo.com>
2021-03-18 18:20:56 -0500

    fixed closed button and fixed opacity problem with modal

--- html-and-css/scripts/scripts.js
+++ html-and-css/scripts/scripts.js
@@ -62,6 +62,7 @@ function zombieprompt() {
 
 const modal = document.querySelector(".modal");
 const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
 closeButton.addEventListener("click", ()=> modal.classList.remove("show_modal"));
 
 function toggleModal() {
@@ -75,7 +76,6 @@ function windowOnClick(event) {
 }
 
 trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
 function screamSound() {
