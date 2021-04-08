df0cbf322b4a97ae8865dbb1b42b0bae3af6424f
David Tetreau <david_tetreau@yahoo.com>
2021-04-08 08:17:11 -0500

    added js and css for dark mode buttons

--- javascript-job-search-mvp/script/script.js
+++ javascript-job-search-mvp/script/script.js
@@ -3,5 +3,6 @@ console.log('Hello, Vets Who Code!');
 const darkMode = () => {
   const body = document.body;
   const button = document.getElementsByClassName('btn-primary');
-  body.classList.toggle("dark-mode");
+  body.classList.toggle("dark-background");
+  button.classList.toggle("dark-buttons");
 }
