439ab9171ba0f68b5e2e46df4c9167ba66ddc5c9
Joe Romine <joe.romine@ufl.edu>
2021-04-08 20:27:07 -0700

    added jquery flags

--- javascript-job-search-mvp/script/script.js
+++ javascript-job-search-mvp/script/script.js
@@ -57,3 +57,88 @@ for(let i=0; i<job.length; i++){
 innergrid[0].innerHTML=jobposting
 
 console.log(innergrid[0].innerHTML)
+
+$(function () {
+  var countries = [
+    {
+      "name": "United States",
+      "code": "US"
+},   
+
+      {
+          "name": "Korea, Republic of",
+          "code": "KR"
+  }
+]
+
+  var countryInput = $(document).find('.countrypicker');
+  var countryList = "";
+
+
+  //set defaults
+  for (i = 0; i < countryInput.length; i++) {
+
+      //check if flag
+      flag = countryInput.eq(i).data('flag');
+      
+      if (flag) {
+          countryList = "";
+          
+          //for each build list with flag
+          $.each(countries, function (index, country) {
+              var flagIcon = "css/flags/" + country.code + ".png";
+              countryList += "<option data-country-code='" + country.code + "' data-tokens='" + country.code + " " + country.name + "' style='padding-left:25px; background-position: 4px 7px; background-image:url(" + flagIcon + ");background-repeat:no-repeat;' value='" + country.name + "'>" + country.name + "</option>";
+          });
+
+          //add flags to button
+          countryInput.eq(i).on('loaded.bs.select', function (e) {
+              var button = $(this).closest('.btn-group').children('.btn');
+              button.hide();
+              var def = $(this).find(':selected').data('country-code');
+              var flagIcon = "css/flags/" + def + ".png";
+              button.css("background-size", '20px');
+              button.css("background-position", '10px 9px');
+              button.css("padding-left", '40px');
+              button.css("background-repeat", 'no-repeat');
+              button.css("background-image", "url('" + flagIcon + "'");
+              button.show();
+          });
+
+          //change flag on select change
+          countryInput.eq(i).on('change', function () {
+              button = $(this).closest('.btn-group').children('.btn');
+              def = $(this).find(':selected').data('country-code');
+              flagIcon = "css/flags/" + def + ".png";
+              button.css("background-size", '20px');
+              button.css("background-position", '10px 9px');
+              button.css("padding-left", '40px');
+              button.css("background-repeat", 'no-repeat');
+              button.css("background-image", "url('" + flagIcon + "'");
+
+          });
+      }else{
+          countryList ="";
+          
+          //for each build list without flag
+          $.each(countries, function (index, country) {
+              countryList += "<option data-country-code='" + country.code + "' data-tokens='" + country.code + " " + country.name + "' value='" + country.name + "'>" + country.name + "</option>";
+          });
+          
+          
+      }
+      
+       //append country list
+      countryInput.eq(i).html(countryList);
+
+
+      //check if default
+      def = countryInput.eq(i).data('default');
+      //if there's a default, set it
+      if (def) {
+          countryInput.eq(i).val(def);
+      }
+
+  }
+
+});
+$('.countrypicker').countrypicker();
