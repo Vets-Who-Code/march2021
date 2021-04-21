7fdb427e7511569824cb27163acabf47eed66d24
David Tetreau <david_tetreau@yahoo.com>
2021-04-20 19:29:30 -0500

    dumb branch

--- javascript-job-search-mvp/scripts/script.js
+++ javascript-job-search-mvp/scripts/script.js
@@ -17,8 +17,7 @@ const jobSearch = document.getElementById("job-search");
 jobSearch.addEventListener("submit", submitButtonEvent);
 //variable for veteran video
 const veteranVideo = document.getElementById("veteran-video");
-//receives objects from the API (array of objects)
-let job = [];
+
 
 // Triggers the function when the search button is clicked
 function submitButtonEvent(event) {
@@ -32,70 +31,29 @@ function submitButtonEvent(event) {
 
   //API data gets pushed into this as a string 
   let jobposting = "";
+  //receives objects from the API (array of objects)
+  let job = [];
+    //Grid    
+  let innergrid = document.getElementById('jobgrid');
  
   // API GET Request
-  fetch('https://swapi.dev/api/people/?').then(response => {
-          if (response.ok) {
-              return response.json();
-          }
-          throw new Error('Network response error.');
-      })
+  fetch('https://swapi.dev/api/people/?').then(response => response.json(), error => console.log(error))
       .then(response => {
-        if(job.length > 0) {
-          job = [];
-          jobposting = "";
-        }
+          clearOutGrid();
       for(let i = 0; i < response.results.length; i++) {
-      job.push({Title: response.results[i].name, Company: response.results[i].height, Location: response.results[i].mass, Remote: response.results[i].hair_color, JobSnippet: response.results[i].skin_color, Salary: response.results[i].eye_color, DateSincePosted: response.results[i].birth_year});
-    }
-  });
- 
-  //Grid    
-  let innergrid = document.getElementsByClassName('jobgrid');
-
-  //test case for 11111 zip code to demonstrate the no search results screen. To be removed. 
-  if (event.currentTarget[0].value[0] == 1) {
-    job = [];
-  }
-
-  //sort the job array
-  job.sort(function (a, b) {
-    return a.sortOrder - b.sortOrder
-  });
-
-  //if the job variable is an empty array, the hidden class is added to the video and removed from the jobgrid div
-  if (job.length > 0) {
-    veteranVideo.classList.add('hidden');
-    document.getElementById("no-results").classList.add('hidden');
-    document.getElementById('jobgrid').classList.remove('hidden');
-  } else {
-    document.getElementById("no-results").classList.remove('hidden');
-    veteranVideo.classList.remove('hidden');
-  }
-
-  //adds a container and items to the jobposting string for each object in the job array
-  for (let i = 0; i < job.length; i++) {
-    jobposting += `<a href="${job[i].ApplicationSite}" target="_blank" rel="noopener noreferrer"><div class="">
-        <div class="grid-container">
-          <div class="grid-item grid-item-1">Title: ${job[i].Title}</div>
-          <div class="grid-item grid-item-2">Company: ${job[i].Company} - ${job[i].Location}</div>
-          <div class="grid-item grid-item-3">Remote: ${job[i].Remote}</div>
-          <div class="grid-item grid-item-4">Job Description: ${job[i].JobSnippet}</div>
-          <div class="grid-item grid-item-5">Salary: ${job[i].Salary}</div>
-          <div class="grid-item grid-item-6">Date Posted: ${job[i].DateSincePosted}</div>
-        </div></a>`
-  }
-
-  innergrid[0].innerHTML = jobposting;
-
-  if (document.body.classList.contains("dark-background")) {
-    document.querySelectorAll(".grid-container").forEach(e => e.classList.add("dark-container"));
-    document.querySelectorAll(".grid-item").forEach(e => e.classList.add("dark-grid"));
-  } else {
-    document.querySelectorAll(".grid-container").forEach(e => e.classList.remove("dark-container"));
-    document.querySelectorAll(".grid-item").forEach(e => e.classList.remove("dark-grid"));
-  }
-}
+        job.push({Title: response.results[i].name, Company: response.results[i].height, Location: response.results[i].mass, Remote: response.results[i].hair_color, JobSnippet: response.results[i].skin_color, Salary: response.results[i].eye_color, DateSincePosted: response.results[i].birth_year});
+      }
+      makeCards(job);
+      makeGridVisible(job)
+      
+      for(let i = 2; i < response.count/10; i++ ) {
+        fetch(`https://swapi.dev/api/people/?page=${i}`).then(steve => steve.json(), error => console.log(error))
+        .then(steve => morePages(steve))
+        makeCards(job);
+        makeGridVisible(job)
+      }
+    })
+}//close submit function
 
 //# sourceMappingURL=bootstrap-select-country.min.js.map
 var countrypicker = function (a) {
@@ -275,3 +233,57 @@ function contactFormSubmit(event) {
 //this listens for the contact form to submit.
 const contactButton = document.getElementById("contact-form");
 contactButton.addEventListener("submit", contactFormSubmit);
+
+//clear out everything from a previous searcg 
+function clearOutGrid() {
+    job = [];
+    jobposting = "";
+    document.getElementById('jobgrid').innerHTML = '';
+}
+
+//adds a container and items to the jobposting string for each object in the job array
+function makeCards(job) {
+    for (let i = 0; i < job.length; i++) {
+    jobposting += `<a href="${job[i].ApplicationSite}" target="_blank" rel="noopener noreferrer"><div class="">
+        <div class="grid-container">
+          <div class="grid-item ${(document.body.classList.contains("dark-background"))?"dark-grid":""} grid-item-1">Title: ${job[i].Title}</div>
+          <div class="grid-item ${(document.body.classList.contains("dark-background"))?"dark-grid":""} grid-item-2">Company: ${job[i].Company} - ${job[i].Location}</div>
+          <div class="grid-item ${(document.body.classList.contains("dark-background"))?"dark-grid":""} grid-item-3">Remote: ${job[i].Remote}</div>
+          <div class="grid-item ${(document.body.classList.contains("dark-background"))?"dark-grid":""} grid-item-4">Job Description: ${job[i].JobSnippet}</div>
+          <div class="grid-item ${(document.body.classList.contains("dark-background"))?"dark-grid":""} grid-item-5">Salary: ${job[i].Salary}</div>
+          <div class="grid-item ${(document.body.classList.contains("dark-background"))?"dark-grid":""} grid-item-6">Date Posted: ${job[i].DateSincePosted}</div>
+        </div></a>`
+  }
+  document.getElementById('jobgrid').innerHTML = jobposting;
+}
+
+function makeGridVisible(job = []) {
+     //if the job variable is an empty array, the hidden class is added to the video and removed from the jobgrid div
+  if (job.length > 0) {
+    veteranVideo.classList.add('hidden');
+    document.getElementById("no-results").classList.add('hidden');
+    document.getElementById('jobgrid').classList.remove('hidden');
+  } else {
+    document.getElementById("no-results").classList.remove('hidden');
+    document.getElementById('jobgrid').classList.add('hidden');
+    veteranVideo.classList.remove('hidden');
+  }
+
+  if (document.body.classList.contains("dark-background")) {
+    document.querySelectorAll(".grid-container").forEach(e => e.classList.add("dark-container"));
+    document.querySelectorAll(".grid-item").forEach(e => e.classList.add("dark-grid"));
+  } else {
+    document.querySelectorAll(".grid-container").forEach(e => e.classList.remove("dark-container"));
+    document.querySelectorAll(".grid-item").forEach(e => e.classList.remove("dark-grid"));
+  }
+}
+
+//
+function morePages(response) {
+  for(let i = 0; i < response.results.length; i++) {
+        job.push({Title: response.results[i].name, Company: response.results[i].height, Location: response.results[i].mass, Remote: response.results[i].hair_color, JobSnippet: response.results[i].skin_color, Salary: response.results[i].eye_color, DateSincePosted: response.results[i].birth_year});
+      }
+      makeCards(job);
+}
+
+console.log(document.querySelectorAll(".grid-container").length)
