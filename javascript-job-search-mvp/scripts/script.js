6e25d080352b2b7c67cfc75413a2ef7a9433dade
David Tetreau <david_tetreau@yahoo.com>
2021-04-28 17:14:33 -0500

    took away inner scrollbar, removed underline from links, added javascript for grab and scroll but it needs work

--- javascript-job-search-mvp/scripts/script.js
+++ javascript-job-search-mvp/scripts/script.js
@@ -99,6 +99,44 @@ function submitButtonEvent(event) {
 	}
 }
 
+// adds grab to scroll functionality 
+const ele = document.querySelectorAll('.grid-item');
+ele.scrollTop = 100;
+ele.scrollLeft = 150;
+
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+const mouseDownHandler = function(e) {
+    pos = {
+        // The current scroll 
+        left: ele.scrollLeft,
+        top: ele.scrollTop,
+        // Get the current mouse position
+        x: e.clientX,
+        y: e.clientY,
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+	ele.style.cursor = 'grabbing';
+    ele.style.userSelect = 'none';
+};
+
+const mouseMoveHandler = function(e) {
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+    const dy = e.clientY - pos.y;
+
+    // Scroll the element
+    ele.scrollTop = pos.top - dy;
+    ele.scrollLeft = pos.left - dx;
+};
+
+const mouseUpHandler = function() {
+    ele.style.cursor = 'grab';
+    ele.style.removeProperty('user-select');
+};
+
 //Dark Mode
 const darkMode = () => {
 	const body = document.body;
