c4123f595e1dc2f8748e8c54042cc5aa294ed22f
Philip Tenteromano <tenteromano.p@gmail.com>
2021-05-21 18:33:01 -0400

    set complete and sorting

---
+++ cs-challenges/sorting/index.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Implement bubbleSort, selectionSort, and mergeSort
+// Give the big O of each afterwards, and why.
+
+const swap = (a, b) => [b, a];
+
+// O(n^2)
+function bubbleSort(arr) {
+  // TODO: implement
+}
+
+// O(n^2)
+function selectionSort(arr) {
+  // TODO: implement
+}
+
+// O(n log n)
+// The bottom two are part of MergeSort
+function mergeSort(arr) {
+  // TODO: implement
+}
+
+function merge(left, right) {
+  // TODO: implement
+}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
