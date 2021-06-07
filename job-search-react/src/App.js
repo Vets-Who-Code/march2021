b75a65153dbee0c109107d94a40b44b55a420dfb
Joe Romine <joe.romine@ufl.edu>
2021-06-07 15:47:40 -0700

    added footer and body

--- job-search-react/src/App.js
+++ job-search-react/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import JobApp from "./components/JobApp";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <JobApp/>
+      <Footer />
     </div>
   );
 }
