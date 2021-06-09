a4d3fd59362f75318eac0912b325c2deb6c214f1
Joe Romine <joe.romine@ufl.edu>
2021-06-09 16:17:44 -0700

    moved components into folders

--- job-search-react/src/App.js
+++ job-search-react/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import JobApp from "./components/JobApp";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import JobApp from "./components/JobApp/JobApp";
 
 function App() {
   return (
