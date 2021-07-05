ba9d5d0217976faba20249ced384fe180ee5bd99
Stephan Lamoureux <sa.lamoureux@gmail.com>
2021-07-04 21:55:16 -0400

    created a regex function that removes HTML tags from the cards

--- job-search-react/src/components/JobApp/Card/Card.js
+++ job-search-react/src/components/JobApp/Card/Card.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
 export default function Card({ isSubmitted, jobData }) {
+	function removeHTML(text) {
+		// this regex removes html tags
+		return text.replace(/<[^>]*(>|$)|&nbsp;|&zwnj;|&raquo;|&laquo;|&gt;/g, '');
+	}
+
 	return (
 		<div className="grid-container">
-			<div className="grid-item grid-item-1">{jobData.title}</div>
+			<div className="grid-item grid-item-1">{removeHTML(jobData.title)}</div>
 			<div className="grid-item grid-item-2">
-				Company: {jobData.company.display_name} -{' '}
-				{jobData.location.display_name}
+				Company: {removeHTML(jobData.company.display_name)} -{' '}
+				{removeHTML(jobData.location.display_name)}
 			</div>
-			<div className="grid-item grid-item-3">Remote: {}</div> {/* TODO!!! */}
+			<div className="grid-item grid-item-3">
+				Remote:
+				{jobData.description.toLowerCase().indexOf('remote') > -1 ||
+				jobData.description.toLowerCase().indexOf('work from home') > -1
+					? ' Yes'
+					: ' No'}
+			</div>
+			{/* TODO!!! */}
 			<div className="grid-item grid-item-4">
-				Job Description: {/* this regex removes html tags from descriptions */}
-				{jobData.description.replace(
-					/<[^>]*(>|$)|&nbsp;|&zwnj;|&raquo;|&laquo;|&gt;/g,
-					''
-				)}
+				Job Description:
+				{removeHTML(jobData.description)}
 			</div>
 			<div className="grid-item grid-item-6">
 				Date Posted: {jobData.created} {/* FORMAT DATE */}
